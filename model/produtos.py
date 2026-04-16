3a7cc0603d7bd17c38c84bd4ac0960082a425240
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-04-16 14:50:24 -0300

    ADD: Rota no index também.

--- model/produtos.py
+++ model/produtos.py
@@ -2,7 +2,7 @@ from database.conexao import Conexao
 
 def rec_destaques():
     con, cur  = Conexao.conectar()
-    cur.execute("SELECT  destaque, url_imagem FROM produto where destaque  = 1;")
+    cur.execute("SELECT  codigo, destaque, url_imagem FROM produto where destaque  = 1;")
     destaques = cur.fetchall()
     con.close()
 
@@ -10,7 +10,7 @@ def rec_destaques():
 
 def rec_produtos_rap():
     con, cur  = Conexao.conectar()
-    cur.execute("SELECT url_imagem, produto, descricao, destaque, preco FROM produto;")
+    cur.execute("SELECT codigo, url_imagem, produto, descricao, destaque, preco FROM produto;")
     produtos = cur.fetchall()
     con.close()
 
