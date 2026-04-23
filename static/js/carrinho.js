a6b0f886e007e27b3dde28f605a19b47d5dfbd90
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-04-23 17:13:43 -0300

    ADD: Carrinho de compras.

---
+++ static/js/carrinho.js
@@ -0,0 +1,29 @@
+    async function  mostrar_carrinho() {
+    const resposta = await fetch("http://10.110.134.2:8080/api/get/carrinho")
+
+    if (!resposta.ok) {
+        alert("ERRO AO CARREGAR CARRINHO!")
+    }
+    else{
+        const dados = await resposta.json()
+
+        const carrinho = document.getElementById("carrinho")
+
+        for (let dado of dados){
+            let linha = `    
+        <div class="cart-item" >    
+            <img src="${dado.imagem}" alt="Hambúrguer" class="cart-item__image">
+        <div class="cart-item__info">
+            <div class="cart-item__title">${dado.nome}</div>
+            <div class="cart-item__price">${dado.preco}</div>
+            <div class="cart-item__description">Pão, hambúrguer 180g, queijo, bacon, alface, tomate e molho especial.</div>
+        </div>
+        </div>
+        `
+        carrinho.innerHTML += linha;
+        };
+    
+    };
+};
+
+mostrar_carrinho();
