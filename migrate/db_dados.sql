0ec9f9e47915a2d2b4d52e4d163ad856072b838d
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-05-08 11:07:28 -0300

    MODIFY: Database.

--- migrate/db_dados.sql
+++ migrate/db_dados.sql
@@ -16,14 +16,14 @@ insert into cardapio.carrinho(
 	usuario, 
     ped_fin
 ) values('Ana Clara', 1);
-INSERT INTO cardapio.itens_car 
-(codigo_item_car,
- codigo_carrinho,
- codigo_produto,
- quantidade)
- VALUES
- ('1',
- '1',
- '1',
- '1')
- ;
+-- INSERT INTO cardapio.itens_car 
+-- (codigo_item_car,
+ -- codigo_carrinho,
+ -- codigo_produto,
+ -- quantidade)
+ -- VALUES
+ -- ('1',
+ -- ' 1',
+ -- '1',
+--  '1')
+ -- ;
